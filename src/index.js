f9e025529c73c9aea733ef098ee442f99041a286
Edward Paler <paler.edward@gmail.com>
2021-11-11 08:59:38 +0800

    Create JSX Element

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+
+  document.getElementById('root')
+); */
+
+const name = 'John Smith';
+const user = {
+  firstName: 'Jane',
+  lastName: 'Doe'
+}
+
+function formatName(user){
+  return user.firstName + " " + user.lastName;
+}
+
+const element = <h1>Hello, {formatName(user)}</h1>
+
+ReactDOM.render(
+  element,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
